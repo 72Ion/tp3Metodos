78a28a5bcb374721789ee7b92b538eaaf9167531
belubeluga <belugotz@gmail.com>
2024-11-01 14:29:23 -0300

    con imagen p rocesada

--- 1_SVD.py
+++ 1_SVD.py
@@ -49,13 +49,15 @@ fig, axs = plt.subplots(len(ks) // 3, len(ks) // 2, figsize=(14, 8))
 axs = axs.flatten()
 errores = []
 
-for ax, k in zip(axs, ks):
-    Xapprox = U[:, :k] @ S[:k, :k] @ VT[:k, :]
+for ax, d in zip(axs, ks):
+    Xapprox = U[:, :d] @ S[:d, :d] @ VT[:d, :]
     error = error_reconstruccion(X, Xapprox)
     errores.append(error)
-    factor = factor_compresion(k, X)
+    factor = factor_compresion(d, X)
     ax.imshow(Xapprox, cmap='gray')
-    ax.set_title(f'k = {k}, Compresión = {round(factor * 100, 2)}%, Error = {round(error * 100, 2)}%')
+    ax.set_title(f'd = {d}, Compresión = {round(factor * 100, 2)}%, Error = {round(error * 100, 2)}%')
+    if d == 50:
+        plt.imsave('imagen_procesada_d50.png', Xapprox, cmap='gray')
 
 plt.show()
 
@@ -63,10 +65,10 @@ plt.show()
 plt.figure(figsize=(10, 5))
 plt.plot(ks, errores, '-o')
 plt.axhline(0.1, color='r', linestyle='--', label='Error del 10%')
-plt.xlabel('k')
+plt.xlabel('d')
 plt.ylabel('Error de reconstrucción (%)')
 plt.yscale('log')
-plt.title('Error de Reconstrucción vs. Número de Componentes k')
+plt.title('Error de Reconstrucción vs. Número de Componentes d')
 plt.legend()
 plt.show()
 
@@ -75,10 +77,10 @@ fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(13, 5))
 ax1.plot(np.diag(S), '-o')
 ax1.set_yscale('log')
 ax1.set_title('Valores Singulares')
-ax1.set_xlabel(r'$k$')
+ax1.set_xlabel(r'$d$')
 ax1.set_ylabel(r'$\sigma$')
 ax2.plot(np.cumsum(np.diag(S)) / np.sum(np.diag(S)))
-ax2.set_xlabel(r'$k$')
+ax2.set_xlabel(r'$d$')
 ax2.set_ylabel(r'$\mathrm{cumsum}(\sigma)/\mathrm{sum}(\sigma)$')
 ax2.set_title('Energía Acumulada')
 
